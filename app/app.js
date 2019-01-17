d3e3a97afcbad00967a38ec6c69ffcdbc8e1269b
iryna <iryna.polishchuk@teeamvoy.com>
2019-01-17 16:06:56 +0200

    Add moment js to package.json.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-// @flow
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
--- app/styles/Colors/index.js
+++ app/styles/Colors/index.js
@@ -2,3 +2,4 @@ export const white = '#ffffff';
 export const lightGrey = 'lightGrey';
 export const grey = 'grey';
 export const primary = '#35568c';
+export const lightblue = 'lightblue';
