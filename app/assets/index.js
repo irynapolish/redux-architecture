da3bf6e8592cfa337276226e8fa0ef7736dc8c16
iryna <iryna.polishchuk@teeamvoy.com>
2019-01-17 15:27:18 +0200

    Delete useless images from assets.

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,12 +1,4 @@
-/*eslint global-require: "off"*/
-
 module.exports = {
-  iconArrowLeft: require('./img/iconArrowLeft.png'),
-  iconClose: require('./img/iconClose.png'),
-  iconDotsHorizontal: require('./img/iconDotsHorizontal.png'),
-  iconPlus: require('./img/iconPlus.png'),
-  iconSearch: require('./img/iconSearch.png'),
-  iconCloud: require('./img/iconCloud.jpg'),
   iconSun: require('./img/iconSun.jpg'),
   iconMoon: require('./img/iconMoon.jpeg'),
   
